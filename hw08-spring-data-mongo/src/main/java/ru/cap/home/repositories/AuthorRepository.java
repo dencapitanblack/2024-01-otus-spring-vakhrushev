43d6839950e86760ec6be6d5e90111bc778fd1f4
dvakhrushev <dencapitanblack@icloud.com>
2024-04-07 23:59:02 +0300

    Initial commit

---
+++ hw08-spring-data-mongo/src/main/java/ru/cap/home/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package ru.cap.home.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.cap.home.models.Author;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
---
+++ hw08-spring-data-mongo/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into author(full_name) values ('author_3');
+insert into comment(comment, book_id) values ('comment_1_1', 1);
+insert into comment(comment, book_id) values ('comment_1_2', 1);
+insert into comment(comment, book_id) values ('comment_1_3', 1);
+insert into comment(comment, book_id) values ('comment_1_4', 1);
